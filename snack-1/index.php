4028888d63fc3055d73e929183b2f4609e8457b0
EmanuelaPau <uapame@gmail.com>
2023-07-03 12:24:17 +0200

    snack 1

--- snack-1/index.php
+++ snack-1/index.php
@@ -1,5 +1,5 @@
-<!-- Dato un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema: -->
-
+<!-- Dato un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema: 
+Olimpia Milano - Cantù | 55-60-->
 <?php
 
 $matches = [
@@ -42,4 +42,24 @@ $matches = [
 ]; 
 ?>
 
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php 
+    foreach($matches as $element){ ?>
+            <li>
+                <p>
+                    <?php echo $element['team1'] . ' - ' . $element['team2'] . ' | ' . $element['point_team_1'] . '-' . $element['point_team_2']; ?> 
+                </p>
+            </li>
+    <?php } ?> 
+    </ul>
+</body>
+</html>
+
