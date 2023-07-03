5ee0bcb1ac894a35dfca4391222d6786562b00d1
EmanuelaPau <uapame@gmail.com>
2023-07-03 12:47:07 +0200

    Add form

---
+++ snack-2/index.php
@@ -0,0 +1,25 @@
+<!-- Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che:
+- name sia più lungo di 3 caratteri,
+- mail contenga un punto e una chiocciola e
+- age sia un numero.
+Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato" -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form check</title>
+</head>
+<body>
+    <form action="" method="GET">
+        <label for="fname">name</label>
+        <input type="text" id="name" name="name"><br><br>
+        <label for="lname">email</label>
+        <input type="mail" id="email" name="mail"><br><br>
+        <label for="lname">age</label>
+        <input type="number" id="age" name="age"><br><br>
+        <input type="submit" value="Submit">
+    </form> 
+</body>
+</html>
