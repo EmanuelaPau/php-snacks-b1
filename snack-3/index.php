ec5732c62badd8c64651e3274fb2c0a3719a99e5
EmanuelaPau <uapame@gmail.com>
2023-07-04 12:28:10 +0200

    Add function

--- snack-3/index.php
+++ snack-3/index.php
@@ -1,4 +1,26 @@
 <?php 
-    $arrayLetters =  [a,b,c,d,e];
-    $arrayNumbers = [1,2,3,4,5]
+    $arrayLetters =  ['a','b','c','d','e'];
+    $arrayNumbers = [1,2,3,4,5];
+    
+    mergeArrays($firstArray, $secondArray) {
+        $mergedArray = [];
+
+        for ($i = 0; $i < count($firstArray); $i++) {
+            $mergedArray[] = $firstArray[$i];
+            $mergedArray[] = $secondArray[$i];
+        }
+        return $mergedArray
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php var_dump(mergeArrays($arrayLetters,  $arrayNumbers )) ?></p>
+</body>
+</html>
