1685c8e24fbb020a221e57da41224d909def6601
EmanuelaPau <uapame@gmail.com>
2023-07-04 12:48:28 +0200

    Add function

--- snack-4/index.php
+++ snack-4/index.php
@@ -1,5 +1,28 @@
-<?php 
+<!-- Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
 
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa 
+tra "a" e "b" -->
+
+<?php 
+    $myArray = 
+    [
+        'gina',
+        'pippo',
+        'paperino',
+        'paperina',
+        'minnie',
+        'topolino',
+        'pluto'
+    ];
+
+    function addSubArray($array, $minNum, $maxNum) {
+        $newArray = [];
+        for($i = $minNum -1 ; $i <= $maxNum -1; $i++) {
+            $newArray[] = $array[$i];
+        }
+        return $newArray;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +33,8 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <p>
+        <?php var_dump(addSubArray($myArray, 3, 6)) ?>
+    </p>
 </body>
 </html>
